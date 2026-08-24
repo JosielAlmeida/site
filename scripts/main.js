fc6dae77af263dc11630f0c6f681f147b8e0bcf1
JosielAlmeida <josielsalmeida@gmail.com>
2026-08-24 12:45:34 -0300

    removi codigo morto e responsividade

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 
 function testeUsuario() {
     if (document.getElementById("emailcadastro").value == "") {
-        document.getElementById("emailerro").innerHTML = "Email é obrigatório";
+        document.getElementById("emailerro").innerHTML = "Atenção: O <span>email</span> é obrigatório!";
     }
     else if (document.getElementById("emailcadastro").value.indexOf("@") == -1) {
         document.getElementById("emailerro").innerHTML = "Email inválido";
